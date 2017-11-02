f14f9b70cbeb6bc9df86e4217f33992e149f4284
Chris Harvey <chrisharvey2pi@gmail.com>
2017-11-02 18:29:54 -0400

    forgot to update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
-var gulp = require('gulp')
-var pug = require('gulp-pug')
-var less = require('gulp-less')
-var autoprefixer = require('gulp-autoprefixer')
-var clean_css = require('gulp-clean-css')
-var sourcemaps = require('gulp-sourcemaps')
+const gulp         = require('gulp')
+const pug          = require('gulp-pug')
+const less         = require('gulp-less')
+const autoprefixer = require('gulp-autoprefixer')
+const clean_css    = require('gulp-clean-css')
+const sourcemaps   = require('gulp-sourcemaps')
 
 gulp.task('pug:docs', function () {
   return gulp.src('docs/{index,phi,code,principles,base,obj,comp-typo,comp-ui,help,atom}.pug')
     .pipe(pug({
       basedir: './',
       locals: {
+        Xmeter: require('xmeter'),
         Docs: require('./docs/_models/Docs.class.js'),
       },
     }))
